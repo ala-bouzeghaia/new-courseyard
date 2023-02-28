3439ba249af7e8d3e381682ec782c7e9c82c19b7
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2023-02-28 13:09:47 +0100

    feat(videos-page): Add notes section and refactored chapters section

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -1,65 +1,46 @@
-import { useState, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import YouTube, { YouTubeProps } from 'react-youtube';
-
-import { ChapterType } from '../types/chapters';
-import Chapter from '../components/Chapter';
 import ChapterList from '../components/ChapterList';
-
+import NoteList from '../components/NoteList';
+import { useChapters } from '../hooks/useChapters';
 import styles from '../styles/Videos.module.scss';
+import { NoteType } from '../types/note';
+import { convertTimecodeToSeconds } from '../utils/helpers';
 
 const videoId = 'w7ejDZ8SWv8';
 
-const convertTimecodeToSeconds = (timecode: string) => {
-  const timeSplit = timecode.split(' ')[0].split(':');
-  // console.log(timeSplit);
-  if (timeSplit.length >= 3) {
-    return Number(timeSplit[0]) * 3600 + Number(timeSplit[1]) * 60 + Number(timeSplit[2]);
-  }
-  return Number(timeSplit[0]) * 60 + Number(timeSplit[1]);
-};
-
 function Videos() {
   const playerRef = useRef({} as YouTube);
-  const [chapters, setChapters] = useState([] as ChapterType[]);
   const [globalPlayerState, setGlobalPlayerState] = useState(2);
   const [currentTime, setCurrentTime] = useState(0);
 
-  //TODO: add api call
-  const getChapters = async () => {
-    //TODO: 1 - api call to get description
-    //TODO: 2 - split description into timecodes & chapters
-    //TODO: 3 - reformat timecodes & chapters
-    //TODO: 4 - store in state
-  };
+  const { chapters, title } = useChapters(videoId, playerRef);
+  const noteList: NoteType[] = [];
 
-  const goToTimecodeFromChapter = (timecode: string) => {
-    const time = convertTimecodeToSeconds(timecode);
-    playerRef.current.internalPlayer.seekTo(time);
-  };
-
-  const startChapter = (chapter: ChapterType) => {
-    goToTimecodeFromChapter(chapter.timeStart);
-    chapter.status = 'ongoing';
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      playerRef.current.internalPlayer?.getCurrentTime().then((res: number) => {
+        setCurrentTime(res);
+      });
+    }, 500);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentTime, playerRef]);
 
   const onPlay: YouTubeProps['onPlay'] = () => {
     setGlobalPlayerState(1);
-    // activeChapter = chapters?.filter(
-    //   (chapter) =>
-    //     currentTime <
-    //       convertTimecodeToSeconds(chapter.timeStart) + chapter.duration &&
-    //     currentTime >= convertTimecodeToSeconds(chapter.timeStart)
-    // )[0];
+    const activeChapter = chapters?.filter(
+      (chapter) =>
+        currentTime < convertTimecodeToSeconds(chapter.timeStart) + chapter.duration &&
+        currentTime >= convertTimecodeToSeconds(chapter.timeStart),
+    )[0];
 
-    // // activeChapter.status = "ongoing";
-    // // if (activeChapter.status !== "completed") {
-    // // }
-
-    // console.log(activeChapter);
-    // if (activeChapter) {
-    //   activeChapter.status = "ongoing";
-    // }
+    if (activeChapter) {
+      activeChapter.status = 'ongoing';
+    }
   };
+
   const onPause: YouTubeProps['onPause'] = () => {
     setGlobalPlayerState(2);
   };
@@ -67,8 +48,9 @@ function Videos() {
   return (
     <main className={styles.container}>
       <div className={styles['video-container']}>
-        <h1>Title</h1>
+        <h1>{title}</h1>
         <YouTube
+          data-testid="video-player"
           videoId={videoId}
           className={styles.player}
           opts={{ width: '100%', height: '100%' }}
@@ -76,9 +58,15 @@ function Videos() {
           onPlay={onPlay}
           onPause={onPause}
         />
-        <div className={styles.notes}>notes</div>
+        <NoteList currentTime={currentTime} playerRef={playerRef} noteList={noteList} />
       </div>
-      <ChapterList chapters={chapters} player={playerRef} />
+      <ChapterList
+        chapters={chapters}
+        playerRef={playerRef}
+        currentTime={currentTime}
+        globalPlayerState={globalPlayerState}
+        setGlobalPlayerState={setGlobalPlayerState}
+      />
     </main>
   );
 }
