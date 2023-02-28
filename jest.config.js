dfa065026c7931e1a15fff7fd0b8b45f43866f20
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2023-02-28 12:51:38 +0100

    chore(jest): Setup jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
+// import nextJest from 'next/jest';
 const nextJest = require('next/jest');
-
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
@@ -13,7 +13,9 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  testEnvironmentOptions: { resources: 'usable' },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// export default createJestConfig(customJestConfig);
 module.exports = createJestConfig(customJestConfig);
