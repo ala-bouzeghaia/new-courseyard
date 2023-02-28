ce4959af6871e75acc1c68cf566ccc0d4add6266
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2023-02-28 13:04:53 +0100

    refactor: Add types

---
+++ types/chapter.ts
@@ -0,0 +1,9 @@
+export type ChapterType = {
+  id: string;
+  title: string;
+  timeStart: string;
+  duration: number;
+  status: string;
+  progression: number;
+  isCompleted: boolean;
+};
---
+++ types/note.ts
@@ -0,0 +1,5 @@
+export type NoteType = {
+  id: string;
+  timecode: number;
+  content: string;
+};
